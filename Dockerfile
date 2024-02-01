d862c19933bb19a6977c7031ade857b084747377
medhakimbedhief <61546101+medhakimbedhief@users.noreply.github.com>
2024-02-01 23:38:00 +0000

    Update in webapp and Dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,12 @@
 FROM python:3.9
 
+RUN python -m pip install --upgrade pip
+
 COPY ./requirements.txt /webapp/requirements.txt
 
 WORKDIR /webapp
 
-RUN pip install-r requirement.txt
+RUN pip install -r requirements.txt
 
 COPY webapp/* /webapp
 
@@ -12,4 +14,4 @@ LABEL authors="medha"
 
 ENTRYPOINT ["uvicorn"]
 
-CMD ["--host", "0.0.0.0", "app:app" ]
+CMD ["--host", "0.0.0.0", "main:app" ]
--- webapp/main.py
+++ webapp/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from fastapi.responses import HTMLResponse
 from pydantic import BaseModel
-from Code.INF import infenrece
+from INF import infenrece
 
 app = FastAPI()
 
@@ -11,7 +11,8 @@ class Body(BaseModel):
 @app.get('/')
 def root():
     return HTMLResponse('<h1> CWI German language')
-@app.post(/simplify)
+@app.post('/simplify')
 def predict(body: Body):
-    detected_words, old_sentence, new_sentence = infenrece(body.text)
+    detected_words, new_sentence , old_sentence = infenrece(body.text)
     return new_sentence
+
